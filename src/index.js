73a3c1b9b8aeac28982cc8a9f6fdcb6de5fdacde
HardMaind <Hardikmaind1802@gmail.com>
2021-11-17 11:32:54 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -9,19 +9,25 @@ import * as serviceWorker from "./serviceWorker";
 import { LayoutProvider } from "./context/LayoutContext";
 import { UserProvider } from "./context/UserContext";
 
-ReactDOM.render(
-  <LayoutProvider>
-    <UserProvider>
-      <ThemeProvider theme={Themes.default}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </UserProvider>
-  </LayoutProvider>,
-  document.getElementById("root"),
+ReactDOM.render( <
+    LayoutProvider >
+    <
+    UserProvider >
+    <
+    ThemeProvider theme = { Themes.default } >
+    <
+    CssBaseline / >
+    <
+    App / >
+    <
+    /ThemeProvider> < /
+    UserProvider > <
+    /LayoutProvider>,
+    document.getElementById("root"),
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
+// Just add into github 
 serviceWorker.unregister();
